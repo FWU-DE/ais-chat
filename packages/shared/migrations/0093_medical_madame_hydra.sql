8843778b44d5e81be77c53a6a26b3f0450039bb9
robin.laerm <robin.laerm@xitaso.com>
2026-09-21 18:05:12 +0200

    add cast to uuid in migration

--- packages/shared/migrations/0093_medical_madame_hydra.sql
+++ packages/shared/migrations/0093_medical_madame_hydra.sql
@@ -18,7 +18,7 @@ ALTER TABLE "community_template_request" ALTER COLUMN "status" SET DATA TYPE tex
 DROP TYPE "public"."template_request_status";--> statement-breakpoint
 CREATE TYPE "public"."template_request_status" AS ENUM('submitted', 'approved', 'rejected', 'cancelled');--> statement-breakpoint
 ALTER TABLE "community_template_request" ALTER COLUMN "status" SET DATA TYPE "public"."template_request_status" USING "status"::"public"."template_request_status";--> statement-breakpoint
-ALTER TABLE "community_template_request" ALTER COLUMN "created_by" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "community_template_request" ALTER COLUMN "created_by" SET DATA TYPE uuid USING "created_by"::uuid;--> statement-breakpoint
 ALTER TABLE "community_template_request" ALTER COLUMN "status" SET DEFAULT 'submitted';--> statement-breakpoint
 ALTER TABLE "community_template_request_events" ADD CONSTRAINT "community_template_request_events_template_request_id_community_template_request_id_fk" FOREIGN KEY ("template_request_id") REFERENCES "public"."community_template_request"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "community_template_request_events_template_request_id_index" ON "community_template_request_events" USING btree ("template_request_id");--> statement-breakpoint
