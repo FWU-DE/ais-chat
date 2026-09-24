ddd634eb7f99902479250cf0df7402c6e58259a7
stschaller-xitaso <stefanie.schaller@xitaso.com>
2026-09-24 11:34:17 +0200

    TD-1483: use esm import for wavefile

--- packages/ai-core/src/tts/index.ts
+++ packages/ai-core/src/tts/index.ts
@@ -1,2 +1,2 @@
 export type { SpeechGenerationFn, SpeechResponse } from './types';
-export { constructGoogleSpeechGenerationFn } from './providers/google';
+//export { constructGoogleSpeechGenerationFn } from './providers/google';
